4558fd5abe32a9f6521ea3cc256acf1fcda81bd3
COWWARS <127351879+COWWARS@users.noreply.github.com>
2024-08-03 20:44:21 -0400

    Update index.js

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -8,12 +8,11 @@ const ServiceCard = ({ name, description }) => {
   useEffect(() => {
     setMounted(true);
   }, []);
-
   return (
     <div
-      className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-        mounted && theme === "dark" ? "bg-slate-800" : "bg-slate-50"
-      }`}
+      className={w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
+        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
+      } hover:scale-105 link}
     >
       <h1 className="text-3xl">{name ? name : "Heading"}</h1>
       <p className="mt-5 opacity-40 text-xl">
