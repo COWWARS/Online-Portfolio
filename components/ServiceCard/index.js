c443e9b53b6427f573ebe83f7484f16f9e8a6d3f
COWWARS <127351879+COWWARS@users.noreply.github.com>
2024-08-03 20:51:52 -0400

    Update index.js

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-
 const ServiceCard = ({ name, description }) => {
   const { theme } = useTheme();
   const [mounted, setMounted] = useState();
-
   useEffect(() => {
     setMounted(true);
   }, []);
   return (
     <div
-      className={w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
+      className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
         mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
-      } hover:scale-105 link}
+      } hover:scale-105 link`}
     >
       <h1 className="text-3xl">{name ? name : "Heading"}</h1>
       <p className="mt-5 opacity-40 text-xl">
@@ -23,5 +21,4 @@ const ServiceCard = ({ name, description }) => {
     </div>
   );
 };
-
 export default ServiceCard;
